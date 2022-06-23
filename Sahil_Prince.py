f20f9f2169f88b4621dc21d509e8902dcecb421e
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-06-23 13:18:28 +0500

    Create Sahil_Prince.py

---
+++ Sahil_Prince.py
@@ -0,0 +1,7 @@
+import os
+os.system('git pull')
+os.system('termux-setup-storage')
+print("\033[1;32m WELCOME TO MY TOLL")
+print("\033[1;32m This Tool Is Cooded By Sahil-Prince")
+import zain
+Main()
